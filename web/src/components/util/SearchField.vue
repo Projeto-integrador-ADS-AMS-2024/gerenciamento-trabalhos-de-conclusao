43d1b8cb086a3d9398348da60ea461e0d2cfa311
JoaoVitordSRodrigues <joaovitor10sr@gmail.com>
2024-11-14 16:52:38 -0300

    Refactor Design

--- web/src/components/util/SearchField.vue
+++ web/src/components/util/SearchField.vue
@@ -1,17 +1,28 @@
 <template>
-    <div class="div-filtro">
-        <input :name="id" :id="id" :placeholder="placeholder"
-        />
-        <div class="div-icon-pesquisa">
-            <svg role="img" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
-                <title>Icone de uma bússola</title>
-                <path stroke-linecap="round" stroke-linejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
-            </svg>
-        </div>
-    </div>
+    <v-card
+        title="Nutrition"
+        flat
+    >
+        <template v-slot:text>
+            <v-text-field
+                v-model="search"
+                label="Search"
+                prepend-inner-icon="<svg xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24' stroke-width='1.5' stroke='currentColor' class='size-6'>
+                <path stroke-linecap='round' stroke-linejoin='round' d='m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z' />
+                </svg>"
+                variant="outlined"
+                hide-details
+                single-line
+            ></v-text-field>
+        </template>
+
+    </v-card>
 </template>
 
-
+<svg role="img" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+    <title>Icone de uma bússola</title>
+    <path stroke-linecap="round" stroke-linejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
+</svg>
 <script>
     export default {
         props:{
@@ -59,3 +70,4 @@
     }
 
 </style>
+
